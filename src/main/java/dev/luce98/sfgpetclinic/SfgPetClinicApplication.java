8796317a855c15e33d64d8b3a5f1cfb6143b1542
luce98 <r.lucentini@venis.it>
2020-02-14 12:56:17 +0100

    Initial Project Add

---
+++ src/main/java/dev/luce98/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package dev.luce98.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
